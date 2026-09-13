4dc8b8d6666907bcd91f173d53570a3672b2ac80
Harrison Tong <16379581+itstongy@users.noreply.github.com>
2026-09-13 12:42:11 +1000

    Reduce automatic garbage collection retention to 10 days

--- modules/base.nix
+++ modules/base.nix
@@ -24,7 +24,7 @@
     nix.gc = {
       automatic = true;
       dates = "weekly";
-      options = "--delete-older-than 30d";
+      options = "--delete-older-than 10d";
     };
     programs.direnv = {
       enable = true;
